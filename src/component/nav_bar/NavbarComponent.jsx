be89142c7bf07f09ec23298db053f29715d81abd
nabeenasinjalee <nabeenasinjalee@gmail.com>
2024-01-24 16:23:26 +0545

    login and click me

--- src/component/nav_bar/NavbarComponent.jsx
+++ src/component/nav_bar/NavbarComponent.jsx
@@ -2,11 +2,15 @@ function NavBar() {
     return (
         <div className="nav_bar">
             <nav class="nav">
-                <h4 class="nav_logo">logo</h4>
+                <div className="logoSection">
+                    <h4 class="nav_logo">logo</h4>
+                    <h2 className="click">Click me!</h2>
+                </div>
                 <div class="nav_items">
                     <p class="nav_item">Home</p>
                     <p class="nav_item">About</p>
                     <p class="nav_item">Contact</p>
+                    <p class="nav_item">Login</p>
                 </div>
             </nav>
         </div>
